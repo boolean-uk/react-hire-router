903133f9a7c107cba5ef97b41652fbe5ab95e4c3
iliask796 <iliaskal7@gmail.com>
2023-08-23 15:06:56 +0300

    Core Criteria Finished.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { Link, Route, Routes } from "react-router-dom"
 import "./styles.css"
+
+import Dashboard from "./pages/Dashboard"
+import PersonProfile from "./pages/PersonProfile"
 
 export default function App() {
   const [hiredPeople, setHiredPeople] = useState([])
+  const [people, setPeople] = useState([])
+
+  async function getPeople() {
+    const response = await fetch('https://randomuser.me/api/?results=50')
+    const json = await response.json()
+    setPeople([...json.results])
+  }
+  
+  useEffect(() => {
+    getPeople()
+  },[])
 
   return (
-    <>
-      <header>
+    <div className="App">
+      <header>  
         <h1>Hire Your Team</h1>
         <nav>
           <ul>
-            <li>Dashboard</li>
+            <li>
+              <Link to='/'>Dashboard</Link>
+            </li>
           </ul>
         </nav>
       </header>
-    </>
+
+      <Routes>
+        <Route path="/" element={<Dashboard hiredPeople={hiredPeople} people={people} />} />
+        <Route path="/view/:id" element={<PersonProfile hiredPeople={hiredPeople} setHiredPeople={setHiredPeople} />} />
+      </Routes>
+
+    </div>
   )
 }
