cac9a23517b02fab97dd91b0d4f015761774e533
Satoki <satokiito@gmail.com>
2023-11-03 16:27:17 +0000

    core - Dashboard now displays list of 50 people & empty hired list

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import PeopleList from './components/PeopleList'
 
 function Dashboard(props) {
   const { hiredPeople } = props
 
   const [people, setPeople] = useState([])
+
+  // const userURL = "https://randomuser.me/api?results=50"
+  const userURL = "https://randomuser.me/api?inc=name&results=50"
+
+  useEffect(() => {
+    fetch(userURL)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.results)
+      setPeople(data.results)
+    })
+  }, [])
+  
 
   return (
     <main className="dashboard-layout">
