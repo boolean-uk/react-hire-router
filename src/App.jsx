6820483a92711a4c1637c26aea4fb9b2562fdf65
Nazar Tymiv <nazartumiv@gmail.com>
2023-11-03 13:22:24 +0000

    Created route '/' for show dashboard with users and also made fetch request for get 50 peoples from api.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Dashboard from "./pages/Dashboard";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
-  const [hiredPeople, setHiredPeople] = useState([])
+    const [hiredPeople, setHiredPeople] = useState([]);
 
-  return (
-    <>
-      <header>
-        <h1>Hire Your Team</h1>
-        <nav>
-          <ul>
-            <li>Dashboard</li>
-          </ul>
-        </nav>
-      </header>
-    </>
-  )
+    return (
+        <>
+            <header>
+                <h1>Hire Your Team</h1>
+                <nav>
+                    <ul>
+                        <li>Dashboard</li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Dashboard hiredPeople={hiredPeople} />}
+                    />
+                </Routes>
+            </header>
+        </>
+    );
 }
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react'
-import PeopleList from './components/PeopleList'
+import { useEffect, useState } from "react";
+import PeopleList from "./components/PeopleList";
 
-function Dashboard(props) {
-  const { hiredPeople } = props
+function Dashboard({ hiredPeople }) {
+    const [people, setPeople] = useState([]);
 
-  const [people, setPeople] = useState([])
+    useEffect(() => {
+        fetch("https://randomuser.me/api/?results=50")
+            .then((res) => res.json())
+            .then((data) => setPeople(data.results));
+    }, []);
 
-  return (
-    <main className="dashboard-layout">
-      <section>
-        <h2>People</h2>
-        <PeopleList people={people} />
-      </section>
-      <section>
-        <h2>Hired People</h2>
-        <PeopleList people={hiredPeople} />
-      </section>
-    </main>
-  )
+    return (
+        <main className="dashboard-layout">
+            <section>
+                <h2>People</h2>
+                <PeopleList people={people} />
+            </section>
+            <section>
+                <h2>Hired People</h2>
+                <PeopleList people={hiredPeople} />
+            </section>
+        </main>
+    );
 }
 
-export default Dashboard
+export default Dashboard;
