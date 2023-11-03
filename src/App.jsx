bab46e3495d85d118f671c4f97558e7a40be9743
Jack Miller <jdmiller1991@outlook.com>
2023-11-03 15:45:37 +0000

    Project began

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Routes, Route, Link } from 'react-router-dom'
 import { useState } from 'react'
+import Dashboard from './pages/Dashboard'
+import PersonProfile from './pages/PersonProfile'
 import './App.css'
 
 export default function App() {
   const [hiredPeople, setHiredPeople] = useState([])
 
+  
+
   return (
     <>
+    Browser
       <header>
         <h1>Hire Your Team</h1>
         <nav>
--- src/pages/PersonProfile/index.jsx
+++ src/pages/PersonProfile/index.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
 import HireForm from './components/HireForm'
+import PeopleList from '../Dashboard/components/PeopleList'
 
 function PersonProfile(props) {
   const [person, setPerson] = useState(null)
 
   if (!person) return <p>Loading...</p>
+
+useEffect(() => {
+  fetch('https://randomuser.me/api/?results=50')
+  .then(res => res.json())
+  .then(data => setPeople(data.results))
+}, [] )
+
+
 
   return (
     <article>
