a997ff4328af67115d0cdbcb900a3216c8bd6dfd
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2023-11-06 22:57:19 +0100

    exercise done

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import PeopleList from './components/PeopleList'
+import { useEffect, useState } from "react";
+import PeopleList from "./components/PeopleList";
 
-function Dashboard(props) {
-  const { hiredPeople } = props
+function Dashboard({ hiredPeople }) {
+  //const { hiredPeople } = props
 
-  const [people, setPeople] = useState([])
+  const [people, setPeople] = useState([]);
+  useEffect(() => {
+    fetch("https://randomuser.me/api?inc=name,id&results=50")
+      .then((response) => response.json())
+      .then((data) => {
+        setPeople(data.results);
+      });
+  }, []);
 
   return (
     <main className="dashboard-layout">
@@ -17,7 +24,7 @@ function Dashboard(props) {
         <PeopleList people={hiredPeople} />
       </section>
     </main>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
