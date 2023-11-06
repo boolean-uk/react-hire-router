66d6608e7ad35234b04308cff907f04022e5bc7b
Pierluigi <pierluigi.capirci89@gmail.com>
2023-11-06 19:39:05 +0000

    rendered all the people and giving them an offer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import { Link, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import PersonProfile from './pages/PersonProfile'
 
 export default function App() {
   const [hiredPeople, setHiredPeople] = useState([])
@@ -10,9 +13,13 @@ export default function App() {
         <h1>Hire Your Team</h1>
         <nav>
           <ul>
-            <li>Dashboard</li>
+            <Link to="/">Dashboard</Link>
           </ul>
         </nav>
+        <Routes>
+          <Route path="/" element={<Dashboard hiredPeople={hiredPeople}  />} />
+          <Route path="/view/:id" element={<PersonProfile />} />
+        </Routes>
       </header>
     </>
   )
--- src/pages/Dashboard/components/PeopleListItem.jsx
+++ src/pages/Dashboard/components/PeopleListItem.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom"
+
 function PeopleListItem(props) {
   const { person } = props
+
 
   return (
     <li>
       <h3>
-        {person.name.first} {person.name.last}
+        <Link to={`/view/${person.login.uuid}`} state={{data:person}}>{person.name.first} {person.name.last}</Link>
       </h3>
       {person.wage && <p>Wage: £{person.wage}</p>}
     </li>
