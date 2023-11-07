73bf710a609133505ac8adea9ba5804ac1b10c11
Papi <test@Tests-MacBook-Air.fritz.box>
2023-11-07 14:58:53 +0100

    Papi

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 import PeopleList from './components/PeopleList'
+import { useEffect } from 'react'
+
+
+
+const url = "https://randomuser.me/api/?results=50"
 
 function Dashboard(props) {
   const { hiredPeople } = props
 
   const [people, setPeople] = useState([])
+  const fetchData = () => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setPeople(data.results))
+  }
+  useEffect(fetchData, [])
 
   return (
     <main className="dashboard-layout">
--- src/pages/PersonProfile/components/HireForm.jsx
+++ src/pages/PersonProfile/components/HireForm.jsx
@@ -1,11 +1,31 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function HireForm(props) {
   const [wage, setWage] = useState(0)
+  const {person, hiredPeople, setHiredPeople} = props
+  const navigate = useNavigate()
+
 
   function handleSubmit(event) {
     event.preventDefault()
   }
+  const hiredPerson = {...person, wage: wage }
+
+const isClone = hiredPeople.some(person => 
+  person.id.name === hiredPerson.id.name
+)
+
+
+
+
+if (isClone) {
+  // Handle if the person is already hired
+  setMessage('This person is already hired!');
+} else {
+  setHiredPeople([...hiredPeople, hiredPerson]);
+  navigate("/");
+}
 
   return (
     <form onSubmit={handleSubmit}>
@@ -20,6 +40,6 @@ function HireForm(props) {
       <button type="submit">Hire</button>
     </form>
   )
-}
+  }
 
 export default HireForm
