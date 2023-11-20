59adeed1fd824f7b84a5a808d8bb2b473e4b8771
irlydo <fokum8@gmail.com>
2023-11-20 15:07:02 +0000

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { Routes, Route, Link } from 'react-router-dom'
 import { useState } from 'react'
 import './App.css'
+import Dashboard from './pages/Dashboard'
+import PersonProfile from './pages/PersonProfile'
 
 export default function App() {
   const [hiredPeople, setHiredPeople] = useState([])
@@ -10,10 +13,20 @@ export default function App() {
         <h1>Hire Your Team</h1>
         <nav>
           <ul>
-            <li>Dashboard</li>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+             <Link to="/Dashboard">Dashboard</Link>
+            </li>
           </ul>
         </nav>
       </header>
+      <Routes>
+        <Route path='/Dashboard' element={<Dashboard hiredPeople={hiredPeople}/>}/>
+        <Route path="/view/:id" element={<PersonProfile hiredPeople={hiredPeople} setHiredPeople={setHiredPeople}/>}/>
+        <Route path='/' element={<home/>}/>
+      </Routes>
     </>
   )
 }
