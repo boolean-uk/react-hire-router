f7065c75059f0b4a72c5951b4c32a57359432a67
Klara Andersson <klara.andersson@se.experis.com>
2024-02-28 11:46:36 +0100

    Core

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import { useState } from 'react'
 import './App.css'
+import { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import PersonProfile from './pages/PersonProfile'
 
 export default function App() {
   const [hiredPeople, setHiredPeople] = useState([])
+  const [people, setPeople] = useState([])
+
+  const addToHired = (data) =>
+  {
+    setHiredPeople([...hiredPeople, data.person])
+  }
+
+  useEffect(() =>
+  {
+    fetch("https://randomuser.me/api/?results=50")
+    .then((response) => response.json())
+    .then((data) =>
+    {
+      //console.log("DATA", data)
+      setPeople(data.results)
+    })}, [])
 
   return (
     <>
@@ -14,6 +33,11 @@ export default function App() {
           </ul>
         </nav>
       </header>
+
+      <Routes>
+        <Route path='/dashboard' element={<Dashboard hiredPeople={hiredPeople} people={people} />} />
+        <Route path='/dashboard/view/:id' element={<PersonProfile people={people} addToHired={addToHired}/>} />
+      </Routes>
     </>
   )
 }
--- src/pages/Dashboard/components/PeopleListItem.jsx
+++ src/pages/Dashboard/components/PeopleListItem.jsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom'
+
 function PeopleListItem(props) {
-  const { person } = props
+  const { person, index } = props
 
   return (
     <li>
       <h3>
         {person.name.first} {person.name.last}
       </h3>
+      <Link to={`/dashboard/view/${index}`}>View Profile</Link>
       {person.wage && <p>Wage: £{person.wage}</p>}
     </li>
   )
