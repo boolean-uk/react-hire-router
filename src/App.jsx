c779b3f7a9d5691adfe9da54cf58edb61b15a012
Gudbrand <gudbrandwd@live.no>
2024-02-29 14:02:47 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,57 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import PersonProfile from "./pages/PersonProfile/index";
 
 export default function App() {
-  const [hiredPeople, setHiredPeople] = useState([])
+    const [hiredPeople, setHiredPeople] = useState([]);
+    const [people, setPeople] = useState([]);
 
-  return (
-    <>
-      <header>
-        <h1>Hire Your Team</h1>
-        <nav>
-          <ul>
-            <li>Dashboard</li>
-          </ul>
-        </nav>
-      </header>
-    </>
-  )
+    const getData = async () => {
+        const response = await fetch("https://randomuser.me/api/?results=50");
+        const data = await response.json();
+        for (let i = 0; i < data.results.length; i++) {
+            data.results[i].id = i;
+        }
+        console.log(data.results);
+        setPeople([...data.results]);
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <div className="app">
+            <header>
+                <h1>Hire Your Team</h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Dashboard</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <Dashboard hiredPeople={hiredPeople} people={people} />
+                    }
+                />
+                <Route
+                    path="/view/:id"
+                    element={
+                        <PersonProfile
+                            people={people}
+                            hiredPeople={hiredPeople}
+                            setHiredPeople={setHiredPeople}
+                        />
+                    }
+                />
+            </Routes>
+        </div>
+    );
 }
--- src/pages/Dashboard/components/PeopleListItem.jsx
+++ src/pages/Dashboard/components/PeopleListItem.jsx
@@ -1,14 +1,19 @@
-function PeopleListItem(props) {
-  const { person } = props
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
 
-  return (
-    <li>
-      <h3>
-        {person.name.first} {person.name.last}
-      </h3>
-      {person.wage && <p>Wage: £{person.wage}</p>}
-    </li>
-  )
+function PeopleListItem(props) {
+    const { person } = props;
+
+    return (
+        <li>
+            <h3>
+                <Link to={"/view/" + person.id}>
+                    {person.name.first} {person.name.last}
+                </Link>
+            </h3>
+            {person.wage && <p>Wage: £{person.wage}</p>}
+        </li>
+    );
 }
 
-export default PeopleListItem
+export default PeopleListItem;
