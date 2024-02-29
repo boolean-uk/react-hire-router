9fca3828187453e3d397f34760b0b161ea332f0e
Kanthee Kosolyuthasarn <Kanthee>
2024-02-29 15:01:13 +0100

    Kanthee Kosolyuthasarn - C#3 - 29.02.24

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import { Route, Routes, Link } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import PersonProfile from './pages/PersonProfile';
 
 export default function App() {
   const [hiredPeople, setHiredPeople] = useState([])
@@ -10,10 +13,27 @@ export default function App() {
         <h1>Hire Your Team</h1>
         <nav>
           <ul>
-            <li>Dashboard</li>
+            <li className='dashboard'>
+              <Link to={"/"}>Dashboard</Link>
+            </li>
           </ul>
         </nav>
       </header>
+
+      {/* Setting up routes references: */}
+      <Routes>
+        {/* 1. Dashboard: */}
+        <Route 
+          path='/'
+          element={<Dashboard hiredPeople={hiredPeople}/>}
+        />
+        {/* 2. PersonProfile: */}
+        <Route 
+          path='/view/:id'
+          element={<PersonProfile hiredPeople={hiredPeople} setHiredPeople={setHiredPeople}/>}
+        />
+      </Routes>
     </>
   )
 }
+
--- src/pages/PersonProfile/components/HireForm.jsx
+++ src/pages/PersonProfile/components/HireForm.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function HireForm(props) {
+  const {person, hiredPeople, setHiredPeople} = props
   const [wage, setWage] = useState(0)
+  const navigate = useNavigate()
 
   function handleSubmit(event) {
     event.preventDefault()
+    // Spread person to hiredPeople and also add wage attribute:
+    setHiredPeople([...hiredPeople, {...person, wage}]) 
+    
+    navigate('/')   // Switch to dashboard
   }
 
   return (
