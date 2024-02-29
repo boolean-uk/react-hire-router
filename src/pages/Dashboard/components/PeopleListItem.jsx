8112edd763cb20435450c251b5a81ee36b30e757
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2024-02-29 14:21:58 +0100

    core

--- src/pages/Dashboard/components/PeopleListItem.jsx
+++ src/pages/Dashboard/components/PeopleListItem.jsx
@@ -1,12 +1,16 @@
+import {
+  Link,
+} from "react-router-dom";
 function PeopleListItem(props) {
   const { person } = props
 
   return (
-    <li>
+    <li className= "person-item">
       <h3>
         {person.name.first} {person.name.last}
       </h3>
       {person.wage && <p>Wage: £{person.wage}</p>}
+      <Link to={`/view/${person.login.uuid}`}>See profile</Link>
     </li>
   )
 }
--- src/pages/PersonProfile/index.jsx
+++ src/pages/PersonProfile/index.jsx
@@ -1,17 +1,20 @@
+import { useParams } from 'react-router-dom'
 import { useState } from 'react'
 import HireForm from './components/HireForm'
 
 function PersonProfile(props) {
-  const [person, setPerson] = useState(null)
+  const {people, setHiredPeople} = props
+  const {id} = useParams()
+  const person = people.find((person) => person.login.uuid === id);
 
   if (!person) return <p>Loading...</p>
 
   return (
-    <article>
+    <article className="person-profile">
       <h2>
         {person.name.first} {person.name.last}
       </h2>
-      <HireForm person={person} />
+      <HireForm person={person} setHiredPeople={setHiredPeople}/>
     </article>
   )
 }
