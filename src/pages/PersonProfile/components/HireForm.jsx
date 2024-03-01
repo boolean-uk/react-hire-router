1cbb959232cae09589bea57a40034ac114a7347f
Sebastian <sebastian.hanssen@outlook.com>
2024-03-01 11:37:09 +0100

    finish exercise

--- src/pages/PersonProfile/components/HireForm.jsx
+++ src/pages/PersonProfile/components/HireForm.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
 
 function HireForm(props) {
+
+  const { hirePerson, editPerson, hired, navigateToDashboard} = props
+
   const [wage, setWage] = useState(0)
 
   function handleSubmit(event) {
     event.preventDefault()
+    if(hired === false){
+      hirePerson(wage)
+    } else if(hired === true){
+      editPerson(wage)
+    }
+    navigateToDashboard();
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="wage">Wage Offer</label>
+      {hired === false && <label htmlFor="wage">Wage Offer</label>}
+      {hired === true && <label htmlFor="wage">Edit Wage</label>}
       <input
         type="text"
         id="wage"
@@ -17,7 +27,8 @@ function HireForm(props) {
         onChange={e => setWage(e.target.value)}
         value={wage}
       />
-      <button type="submit">Hire</button>
+      {hired === false && <button type="submit">Hire</button>}
+      {hired === true && <button type="submit">Edit</button>}
     </form>
   )
 }
--- src/pages/PersonProfile/index.jsx
+++ src/pages/PersonProfile/index.jsx
@@ -1,17 +1,49 @@
-import { useState } from 'react'
+import { useParams } from 'react-router-dom'
 import HireForm from './components/HireForm'
 
+
 function PersonProfile(props) {
-  const [person, setPerson] = useState(null)
 
-  if (!person) return <p>Loading...</p>
+  const { people, setPeople, hiredPeople, setHiredPeople, navigateToDashboard} = props
+  const { id } = useParams();
+  const person = people.find(person => person.id === parseInt(id));
+  const hiredPerson = hiredPeople.find(person => person.id === parseInt(id));
 
-  return (
+  if (!person && !hiredPerson) return <p>Loading...</p>
+
+  function hirePerson(wage){
+    if(!hiredPeople.includes(person)){
+      person.hired = true
+      person.wage = wage
+      setPeople(x => { return x.filter(y => y !== person)
+        })
+      
+      setHiredPeople([...hiredPeople, person])
+    }
+  }
+
+  function editPerson(wage){
+    const newHiredPeople = [...hiredPeople]
+    const newPerson = newHiredPeople.find(person => person.id === parseInt(id))
+    newPerson.wage = wage
+    setHiredPeople(newHiredPeople)
+  }
+
+  if (person) return (
     <article>
       <h2>
         {person.name.first} {person.name.last}
       </h2>
-      <HireForm person={person} />
+      <HireForm hirePerson = {hirePerson} hired = {person.hired} navigateToDashboard = {navigateToDashboard}/>
+    </article>
+  )
+
+  if (hiredPerson) return (
+    <article>
+      <h2>
+        Employee {hiredPerson.name.first} {hiredPerson.name.last}
+      </h2>
+      <HireForm editPerson = {editPerson} hired = {hiredPerson.hired} navigateToDashboard = {navigateToDashboard}/>
     </article>
   )
 }
