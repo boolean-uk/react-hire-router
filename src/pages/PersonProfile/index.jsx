e834b3fde958ce297e22908597fea9f0ed836c67
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-03-04 09:56:46 +0100

    Implement map view of people

--- src/pages/PersonProfile/index.jsx
+++ src/pages/PersonProfile/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import HireForm from "./components/HireForm";
 import { useLocation } from "react-router-dom";
 
@@ -12,6 +13,11 @@ function PersonProfile() {
       <h2>
         {person.name.first} {person.name.last}
       </h2>
+      {/* Skjalg Eide Hodneland showed me this simple way of querying google maps to display a map */}
+      <iframe
+        style={{ width: "600px", height: "400px" }}
+        src={`https://maps.google.com/maps?q=${person.location.coordinates.latitude},${person.location.coordinates.longitude}&output=embed`}
+      />
       <HireForm person={person} />
     </article>
   );
