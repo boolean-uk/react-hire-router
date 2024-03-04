946490f947b8a3231398d1f0544dc17ea6bebc64
Stian Andreas Ytterstad Nordvik <stian.andreas.ytterstad.nordvik@no.experis.com>
2024-03-04 14:15:23 +0100

    Stian Andreas Ytterstad Nordvik C#3

--- src/pages/PersonProfile/index.jsx
+++ src/pages/PersonProfile/index.jsx
@@ -1,8 +1,11 @@
-import { useState } from 'react'
+import { useParams } from "react-router-dom"
 import HireForm from './components/HireForm'
+import PropTypes from "prop-types"
 
-function PersonProfile(props) {
-  const [person, setPerson] = useState(null)
+function PersonProfile({ people, hiredPeople, setHiredPeople }) {
+  console.log(hiredPeople)
+  const { id } = useParams();
+  const person = people.find(person => person.login.uuid === id);
 
   if (!person) return <p>Loading...</p>
 
@@ -11,9 +14,14 @@ function PersonProfile(props) {
       <h2>
         {person.name.first} {person.name.last}
       </h2>
-      <HireForm person={person} />
+      <HireForm person={person} hiredPeople={hiredPeople} setHiredPeople={setHiredPeople}/>
     </article>
   )
 }
+
+PersonProfile.propTypes = {
+  hiredPeople: PropTypes.array.isRequired,
+}
+
 
 export default PersonProfile
