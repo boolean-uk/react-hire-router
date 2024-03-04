f58d66d140f1d54d7e8837a375a0dd4993897cb3
noahlenn <noahlenn@hotmail.com>
2024-03-04 21:18:16 +0100

    core

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import PersonProfile from './pages/PersonProfile'
 import './App.css'
+
 
 export default function App() {
   const [hiredPeople, setHiredPeople] = useState([])
+  const [people, setPeople] = useState([])
+  
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=5')
+      .then(respone => respone.json())
+      .then(data => {
+        const peopleUpdated = data.results.map((person) => ({
+          ...person,
+          wage: Math.floor(Math.random() * (1000 - 100 + 1)) + 100
+        }));
+        setPeople(peopleUpdated)
+      })
+  }, [])
+
+
+
+  console.log("Hired: ", hiredPeople)
 
   return (
     <>
@@ -10,10 +31,23 @@ export default function App() {
         <h1>Hire Your Team</h1>
         <nav>
           <ul>
-            <li>Dashboard</li>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
           </ul>
         </nav>
       </header>
+      <Routes>
+        <Route
+          path="/" 
+          element={<Dashboard hiredPeople={hiredPeople} people={people} setPeople={setPeople}/>}
+        />
+        <Route
+          path="/view/:id"
+          element={<PersonProfile people={people} hiredPeople={hiredPeople} setHiredPeople={setHiredPeople}/>}
+        />
+
+      </Routes>
     </>
   )
 }
--- src/pages/Dashboard/components/PeopleListItem.jsx
+++ src/pages/Dashboard/components/PeopleListItem.jsx
@@ -1,12 +1,19 @@
+import { Link} from 'react-router-dom'
+
+
 function PeopleListItem(props) {
+  console.log("Inside PeopleListItem: ", { props });
+
   const { person } = props
+
 
   return (
     <li>
       <h3>
         {person.name.first} {person.name.last}
+        <Link to={`/view/${person.name.first+person.name.last}`}>View Profile</Link>
       </h3>
-      {person.wage && <p>Wage: £{person.wage}</p>}
+      {person.wage && <p>Wage: {person.wage}</p>}
     </li>
   )
 }
