8dd8846a8917716d5e3a7399a1da160c4f23a45f
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-03-04 11:29:48 +0100

    Initial people list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import './App.css'
 
 export default function App() {
   const [hiredPeople, setHiredPeople] = useState([])
+  const [people, setPeople] = useState([])
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=50")
+      .then(response => response.json())
+      .then(setPeople)
+  }, [])
+
+  console.log("People is ", people.results )
 
   return (
     <>
@@ -13,6 +23,15 @@ export default function App() {
             <li>Dashboard</li>
           </ul>
         </nav>
+        <main>
+          <h2>Available for hire</h2>
+          {people.results.map((person, index) =>
+            <li key={index}>
+            <Link key={index} to={"/view/:id"}>{`${person.name.title} ${person.name.first} ${person.name.last}`}</Link>
+            </li>
+          )
+        }
+        </main>
       </header>
     </>
   )
