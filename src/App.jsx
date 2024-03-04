e7ae39a3fd198a986667dfdae80860ec15cbd6b2
Petter Karstensen <petterkarstensen@gmail.com>
2024-03-04 15:13:57 +0100

    core and extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { Route, Routes, Link } from 'react-router-dom';
 import './App.css'
+import Dashboard from './pages/Dashboard/index.jsx'
+import PersonProfile from './pages/PersonProfile/index.jsx'
 
 export default function App() {
   const [hiredPeople, setHiredPeople] = useState([])
+  const [people, setPeople] = useState([])
+  
+  useEffect(() => {
+  fetch("https://randomuser.me/api/?results=50")
+    .then((response) => response.json())
+    .then((data) => setPeople(data.results));
+    }, []);
+
+  useEffect(() => {
+  }, [hiredPeople])
 
   return (
     <>
@@ -10,10 +23,22 @@ export default function App() {
         <h1>Hire Your Team</h1>
         <nav>
           <ul>
-            <li>Dashboard</li>
+            <li>
+            <Link to="/">Dashboard</Link>
+              </li>
           </ul>
         </nav>
       </header>
+      <Routes>
+      <Route
+          path="/"
+          element={<Dashboard hiredPeople={hiredPeople} people={people}/>}
+        />
+      <Route
+          path="/view/:id"
+          element={<PersonProfile people={people} hiredPeople={hiredPeople} setHiredPeople={setHiredPeople} />}
+        />
+    </Routes>
     </>
   )
 }
