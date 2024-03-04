795bb22615085d71a48b6f9f7b75447c0cdf928b
Oneallane <oneallane2808@gmail.com>
2024-03-04 10:39:14 +0100

    core + ext

--- src/pages/Dashboard/components/PeopleListItem.jsx
+++ src/pages/Dashboard/components/PeopleListItem.jsx
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom'
 function PeopleListItem(props) {
   const { person } = props
 
   return (
     <li>
-      <h3>
-        {person.name.first} {person.name.last}
-      </h3>
-      {person.wage && <p>Wage: £{person.wage}</p>}
-    </li>
+    <h3>
+      {person.name.first} {person.name.last}
+    </h3>
+    {person.hired === false && <Link to={"/view/" + person.id}>View Profile</Link>}
+    {person.hired === true && <Link to={"/view/" + person.id}>Edit</Link>}
+    {person.hired === true && <p>Wage: £{person.wage}</p>}
+  </li>
   )
 }
 
