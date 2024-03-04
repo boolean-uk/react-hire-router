b0b860df88f115ef163d456bbbeab15d2cab4de3
Martin Skivenesvåg Johannessen <martin.johannessen@no.experis.com>
2024-03-04 09:31:17 +0100

    core + extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import PersonProfile from './pages/PersonProfile';
 
 export default function App() {
+  const [people, setPeople] = useState([])
   const [hiredPeople, setHiredPeople] = useState([])
+
+  const address = 'https://randomuser.me/api/?results=70'
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetch(address)
+        .then(res => res.json())
+        .then(data => {
+          // some fetched people don't have ids for some reason
+          const filteredPeople = data.results.filter(person => person.id && person.id.value !== null).slice(0, 50);
+          setPeople(filteredPeople);
+        })
+        .catch(error => console.error("Fetching people failed:", error));
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
 
   return (
     <>
@@ -10,10 +32,25 @@ export default function App() {
         <h1>Hire Your Team</h1>
         <nav>
           <ul>
-            <li>Dashboard</li>
+            <Link to="/">Dashboard</Link>
           </ul>
         </nav>
       </header>
+      <Routes>
+        <Route path="/" element={
+          <Dashboard
+            people={people}
+            setPeople={setPeople}
+            hiredPeople={hiredPeople}
+          />} />
+        <Route path="/view/:id" element={
+          <PersonProfile
+            people={people}
+            hiredPeople={hiredPeople}
+            setPeople={setPeople}
+            setHiredPeople={setHiredPeople}
+          />} />
+      </Routes>
     </>
   )
 }
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,23 +1,29 @@
-import { useState } from 'react'
 import PeopleList from './components/PeopleList'
 
-function Dashboard(props) {
-  const { hiredPeople } = props
+function Dashboard({ hiredPeople, people }) {
 
-  const [people, setPeople] = useState([])
-
-  return (
-    <main className="dashboard-layout">
-      <section>
-        <h2>People</h2>
-        <PeopleList people={people} />
-      </section>
-      <section>
-        <h2>Hired People</h2>
-        <PeopleList people={hiredPeople} />
-      </section>
-    </main>
-  )
+  {
+    console.log(people)
+    console.log(hiredPeople)
+    return (
+      <main className="dashboard-layout">
+        <section>
+          <h2>People</h2>
+          {people && <PeopleList people={people} />}
+        </section>
+        <section>
+          <h2>Hired People</h2>
+          {hiredPeople && < PeopleList people={hiredPeople} />}
+        </section>
+      </main>
+    )
+  }
 }
 
+import PropTypes from 'prop-types'
+Dashboard.propTypes = {
+  setPeople: PropTypes.func,
+  hiredPeople: PropTypes.arrayOf(PropTypes.object),
+  people: PropTypes.arrayOf(PropTypes.object)
+}
 export default Dashboard
