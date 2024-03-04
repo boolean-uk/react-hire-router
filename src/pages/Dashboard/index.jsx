453b5ee334ea4fdac9acfe64074e424fc68d698a
Osamah Al-maliki <osamah.al-maliki@no.experis.com>
2024-03-04 16:10:23 +0100

    Finished core and extension

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import PeopleList from './components/PeopleList'
+import PeopleList from "./components/PeopleList";
 
-function Dashboard(props) {
-  const { hiredPeople } = props
-
-  const [people, setPeople] = useState([])
+function Dashboard({ hiredPeople, people }) {
 
   return (
     <main className="dashboard-layout">
@@ -17,7 +13,7 @@ function Dashboard(props) {
         <PeopleList people={hiredPeople} />
       </section>
     </main>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- src/pages/PersonProfile/components/HireForm.jsx
+++ src/pages/PersonProfile/components/HireForm.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
-function HireForm(props) {
-  const [wage, setWage] = useState(0)
+function HireForm({ person, setHiredPeople, hiredPeople, setIsHired }) {
+  const [wage, setWage] = useState(0);
+
+  const navigate = useNavigate();
+  const { index } = useParams();
 
   function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
+
+    // Create a new object with the updated wage
+    const updatedPerson = { ...person, wage: wage, index: index };
+
+    // Check if the person already exists in hiredPeople
+    const personIndex = hiredPeople.findIndex((p) => p.index === index);
+    if (personIndex !== -1) {
+      // If the person exists, update their wage
+      const updatedHiredPeople = [...hiredPeople];
+      updatedHiredPeople[personIndex] = updatedPerson;
+      setHiredPeople(updatedHiredPeople);
+    } else {
+      // If the person doesn't exist, add them to hiredPeople
+      setHiredPeople([...hiredPeople, updatedPerson]);
+    }
+
+    setIsHired(true);
+    navigate("/Dashboard");
   }
 
   return (
@@ -14,12 +36,12 @@ function HireForm(props) {
         type="text"
         id="wage"
         name="wage"
-        onChange={e => setWage(e.target.value)}
+        onChange={(e) => setWage(e.target.value)}
         value={wage}
       />
       <button type="submit">Hire</button>
     </form>
-  )
+  );
 }
 
-export default HireForm
+export default HireForm;
