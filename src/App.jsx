d2e6a50a3d5c3dea82b44f92cb1820a7da6497e3
Aleksander Kolsrud <kols203@gmail.com>
2024-03-05 15:46:22 +0100

    Finished core

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { Routes, Link, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import PersonProfile from './pages/PersonProfile'
+
+const URL = "https://randomuser.me/api/?results=50"
 
 export default function App() {
   const [hiredPeople, setHiredPeople] = useState([])
+  const [people, setPeople] = useState([])
+
+  useEffect(() => {
+    fetch(URL, {
+      method: "GET"
+  })
+      .then((response) => response.json())
+      .then((data) => {
+          setPeople(data.results);
+          console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }, [])
 
   return (
     <>
@@ -10,10 +28,16 @@ export default function App() {
         <h1>Hire Your Team</h1>
         <nav>
           <ul>
-            <li>Dashboard</li>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
           </ul>
         </nav>
       </header>
+      <Routes>
+        <Route path="/" element={<Dashboard hiredPeople={hiredPeople} people={people} />} />
+        <Route path='/view/:id' element={<PersonProfile setHiredPeople={setHiredPeople} people={people} hiredPeople={hiredPeople} />} />
+      </Routes>
     </>
   )
 }
--- src/pages/PersonProfile/components/HireForm.jsx
+++ src/pages/PersonProfile/components/HireForm.jsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function HireForm(props) {
   const [wage, setWage] = useState(0)
+  const {person, hiredPeople, setHiredPeople} = props
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    setWage(person.wage || 0)
+  }, [person.wage])
 
   function handleSubmit(event) {
     event.preventDefault()
+
+    const peopleIndex = hiredPeople.findIndex(p => p.id === person.id)
+
+    if (peopleIndex !== -1) {
+      const updateHiredPeople = [...hiredPeople]
+      updateHiredPeople[peopleIndex].wage = wage
+      setHiredPeople(updateHiredPeople)
+    } else {
+      setHiredPeople((prevHiredPeople) => [...prevHiredPeople, {...person, wage}])
+    } 
+    navigate('/')
   }
 
   return (
