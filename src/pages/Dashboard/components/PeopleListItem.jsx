c4be93a1f60a2f6186bd2b77ac1012e6cfff52eb
Marcus Palm <marcus.palm@se.experis.com>
2024-03-08 16:46:11 +0100

    working on extensions

--- src/pages/Dashboard/components/PeopleListItem.jsx
+++ src/pages/Dashboard/components/PeopleListItem.jsx
@@ -9,11 +9,11 @@ function PeopleListItem(props) {
       <h3>
         {person.name.first} {person.name.last}
       </h3>
-      <div>{person.wage && <p>Wage: £{person.wage}</p>}</div>
+      <div>
+        {person.wage && <p>Wage: £{person.wage}</p>}
+        {person.wage && <button>Edit</button>}
+      </div>
 
-      {/* <Routes>
-        <Route path="/view/:id" element={<PersonProfile person={person} />} />
-      </Routes>*/}
       <Link to={`/view/${person.login.uuid}`}>Go to my profile</Link>
     </li>
   );
