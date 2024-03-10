3f0091717b9004ba5a95611a4d581dc02eacbf0c
Tayokanch <tayoquadri11@gmail.com>
2024-03-10 09:42:00 +0000

    rendered hireform component

--- src/pages/PersonProfile/index.jsx
+++ src/pages/PersonProfile/index.jsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import HireForm from "./components/HireForm";
+
 function PersonProfile({ hiredPeople, setHiredPeople }) {
   const [person, setPerson] = useState(null);
   const loc = useLocation();
