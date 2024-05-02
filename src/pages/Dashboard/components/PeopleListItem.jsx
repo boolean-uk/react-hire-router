3b8f7c99adc8d0c699b664d5868c3b4b1814118f
Will Baxter <bax.will@googlemail.com>
2024-05-02 16:44:04 +0100

    Added ability to edit wage

--- src/pages/Dashboard/components/PeopleListItem.jsx
+++ src/pages/Dashboard/components/PeopleListItem.jsx
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 
 function PeopleListItem(props) {
   const { person, editable } = props
+
+
   return (
     <li>
       <h3>
      <Link to={`/${person.login.uuid}`}>{person.name.first} {person.name.last}</Link>
       </h3>
       {person.wage && editable && <p>Wage: £{person.wage}</p>}
-      {editable && <button>Edit</button>}
+      {editable && <Link to={`/${person.login.uuid}`}><button>Edit</button></Link>}
     </li>
   )
 }
