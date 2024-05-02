96ca90b44bffae1a043c3a9a59f8153ab8c3e2ba
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-05-02 13:20:42 +0200

    add function to show people on dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import { Link, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
 
 export default function App() {
   const [hiredPeople, setHiredPeople] = useState([])
@@ -10,10 +12,19 @@ export default function App() {
         <h1>Hire Your Team</h1>
         <nav>
           <ul>
-            <li>Dashboard</li>
+            <li><Link to='/dashboard'>Dashboard</Link></li>
           </ul>
         </nav>
       </header>
+
+    <Routes>
+
+      <Route 
+        path='/dashboard'
+        element={<Dashboard hiredPeople={hiredPeople} />}
+      />
+
+    </Routes>
     </>
   )
 }
