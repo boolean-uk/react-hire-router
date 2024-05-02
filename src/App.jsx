c71d05b0c9d1a34888f02cdbe29d5328bfb77242
Will Baxter <bax.will@googlemail.com>
2024-05-02 12:17:34 +0100

    Added routing to dashboard page and a small home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./pages/dashboard";
+import "./App.css";
 
 export default function App() {
-  const [hiredPeople, setHiredPeople] = useState([])
+  const [hiredPeople, setHiredPeople] = useState([]);
 
   return (
     <>
@@ -10,10 +12,21 @@ export default function App() {
         <h1>Hire Your Team</h1>
         <nav>
           <ul>
-            <li>Dashboard</li>
+          <li><Link to="/">
+              Home
+            </Link></li>
+          <li><Link to="/dashboard">
+              Dashboard
+            </Link></li>
           </ul>
         </nav>
       </header>
+
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route path="/" element={<p>Find people to hire!</p>} />
+      </Routes>
     </>
-  )
+  );
 }
