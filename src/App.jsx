7235a1102974f30ae95876849af43285020dd98b
Kyle Vann <kyle.vann1996@icloud.com>
2024-05-05 17:18:22 +0100

    1/2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Dashboard from './pages/Dashboard';
+import PersonProfile from './pages/PersonProfile';
+
+
 
 export default function App() {
   const [hiredPeople, setHiredPeople] = useState([])
@@ -14,6 +19,12 @@ export default function App() {
           </ul>
         </nav>
       </header>
+      <main>
+<Routes>
+  <Route path='/' element={<Dashboard hiredPeople={hiredPeople}/>}/>
+  <Route path='/view/:id' element={<PersonProfile />}/>
+</Routes>
+      </main>
     </>
   )
 }
--- src/pages/PersonProfile/index.jsx
+++ src/pages/PersonProfile/index.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import HireForm from './components/HireForm'
+import { useParams } from 'react-router-dom';
+
+
 
 function PersonProfile(props) {
   const [person, setPerson] = useState(null)
+  let { id } = useParams();
+  async function fetchApiData() {
 
-  if (!person) return <p>Loading...</p>
+    const response = await fetch('https://randomuser.me/api/?results=50')
+    const data = await response.json()
+    console.log(data)
+
+    setPerson(data.results[0])
+  }
+
+useEffect(() => {fetchApiData()}, [])
+
+  if (!person) return <p>Loading...{id}</p>
 
   return (
     <article>
